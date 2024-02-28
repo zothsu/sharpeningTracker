416dc119d137b0afe34ea8d63e884b44e04212a7
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-02-27 16:01:59 -0800

    Style hero page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,14 @@ import './HomePage.css'
 export default function HomePage() {
   return (
     <>
-      <h1>KNIFE SHARPENING </h1>
-      <h1>TRACKER</h1>
+    <div className='gridhero'>
+      <p className='hero top'>Because sharp </p>
+      <p className='hero mid'>knives</p>
+      <p className='hero lower'>save lives</p>
+      <div className='herocontainer'>
+        <p className='copy'> Welcome to an application that is designed to help you keep track of when you last sharpened your knives and what you sharpened them with.</p>
+      </div>
+    </div>
     </>
   )
 }
