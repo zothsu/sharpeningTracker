0e3a41467c55b08d2a73fedf033da289da7060e7
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-03-04 11:51:38 -0800

    change knife detail component to table to display in line

--- src/components/KnifeDetail/KnifeDetails.jsx
+++ src/components/KnifeDetail/KnifeDetails.jsx
@@ -15,18 +15,34 @@ export default function KnifeDetails({knives}) {
   }
 
   return (
-    <>
-      {knives.map((k, idx)=> (
-        <Link to={`/library/${k._id}`}>
-          <div className="knifedetails" key={idx}>
-            <p className="name">Name/Series Name <br/>{k.name}</p>
-            <p className="steel">Steel Type <br/>{k.steel === "Other" ? k.otherSteel : k.steel}</p>
-            <p className="date">Purchase Date <br/>{formatDate(k.purchaseDate)}</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Name/Series Name</th>
+          <th>Steel Type</th>
+          <th>Purchase Date</th>
+          <th>Last Sharpened</th>
+          <th> Delete </th>
+        </tr>
+      </thead>
+      <tbody>
+        {knives.map((k, idx) => (
+          <tr key={idx}>
+            <td>
+              <Link to={`/library/${k._id}`}>
+                {k.name}
+              </Link>
+            </td>
+            <td>{k.steel === "Other" ? k.otherSteel : k.steel}</td>
+            <td>{formatDate(k.purchaseDate)}</td>
+            <td>{k.stones.length > 0 ? formatDate(findLastSharpened(k)) : "N/A"}</td>
+            <td>
+              <button>Delete</button>
+            </td>
 
-            <p className="date">Last Sharpened <br/>{k.stones.length > 0 ? formatDate(findLastSharpened(k)) : "N/A"}</p>
-          </div>
-        </Link>
-      ))}
-    </>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
