99ca674c874fcf9c1a2ccbed1e60c713c5517a86
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-03-11 10:53:13 -0700

    Update styling for homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,8 @@ export default function HomePage() {
   return (
     <>
       <div className='herocontainertop'>
-        <p className='hero top'>Sharp knives</p>
-        <p className='hero lower'>save lives</p>
+        <p className='hero top'>Sharp knives <br/>
+        save lives</p>
         <p className='copy'>Helping you keep track of when you last sharpened your knives and what you sharpened them with.</p>
       </div>
       <Footer />
